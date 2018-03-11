c27eafac9c5f23039c605b0bcc1245ccad0d81e0
Lauren W <lauwong@nuevaschool.org>
2018-03-10 16:07:38 -0800

    Added advanced more storyboarding and connecting viewcontrollers

---
+++ FamilyCheckIn/ContactsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsViewController.swift
+//  FamilyCheckIn
+//
+//  Created by Lauren Wong on 3/10/18.
+//  Copyright © 2018 MenloHacks. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
